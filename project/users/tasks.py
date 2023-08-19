46aedb268cc8e129fbc98ea0d0de7af29f69d6c1
Moldir Auezova <moldirauezova@MacBook-Air-Moldir.local>
2023-08-19 14:29:59 +0600

    websocket with celery

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -1,7 +1,10 @@
 import random
-from celery import shared_task
-from celery.utils.log import get_task_logger
+
 import requests
+from asgiref.sync import async_to_sync
+from celery import shared_task
+from celery.signals import task_postrun
+from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
@@ -37,3 +40,7 @@ def task_process_notification(self):
         raise self.retry(exc=e, countdown=5)
 
 
+@task_postrun.connect
+def task_postrun_handler(task_id, **kwargs):
+    from project.ws.views import update_celery_task_status
+    async_to_sync(update_celery_task_status)(task_id)
